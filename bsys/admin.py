ee86e2d9f9a920329cd300ea3f9e20deefc117e5
khagag <the_lord29@yahoo.com>
2020-02-22 19:40:30 +0200

    [modify] register models to admin

--- bsys/admin.py
+++ bsys/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from django.apps import apps
+
+# auto-register all models in the app "bsys"
+app = apps.get_app_config('bsys')
+
+for model_name, model in app.models.items():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
