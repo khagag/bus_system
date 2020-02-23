2c56413b895293761213b07583d6993a916a0abe
khagag <the_lord29@yahoo.com>
2020-02-23 12:29:26 +0200

    [modify] change database to mysql

--- bus_system/settings.py
+++ bus_system/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'bus_system.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bus_sys',
+        'USER': 'root',
+        'PASSWORD': "kareem",
+        'HOST': "",
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
